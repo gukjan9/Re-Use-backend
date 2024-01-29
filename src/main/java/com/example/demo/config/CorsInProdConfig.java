0e1c3620ad1cbb557dc01b4aba2b28d9b7e56c24
GukJang <9ukjang@gmail.com>
2024-01-29 21:06:10 +0900

    @value import 수정

--- src/main/java/com/example/demo/config/CorsInProdConfig.java
+++ src/main/java/com/example/demo/config/CorsInProdConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.config;
 
 import com.example.demo.util.FilterChainRing;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
